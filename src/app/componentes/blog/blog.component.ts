0246f78b33fdc6f56ddb59b60b1966ba5c32b714
Ramón Armero Pardo <ramonarmeropardo@gmail.com>
2024-02-12 20:07:11 +0100

    Recolección de datos del formulario y validación del contenido

--- src/app/componentes/blog/blog.component.ts
+++ src/app/componentes/blog/blog.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  arrNoticias: any[] = [];
 
+  newNoticia: any = {
+    titular: '',
+    imagen: '',
+    cuerpo: '',
+    fecha: '',
+  };
+
+  enviar() {
+    if (
+      this.newNoticia.titular === '' ||
+      this.newNoticia.imagen === '' ||
+      this.newNoticia.cuerpo === '' ||
+      this.newNoticia.fecha === ''
+    ) {
+      alert('Debes rellenar todos los campos');
+    } else {
+      this.arrNoticias.push(this.newNoticia);
+      this.newNoticia = {
+        titular: '',
+        imagen: '',
+        cuerpo: '',
+        fecha: '',
+      };
+    }
+    console.log(this.arrNoticias);
+  }
+
+  cargarDatos(): string {
+    return `<div>
+    <h1></h1>
+    <img></img>
+    <p></p>
+    <p></p>
+    </div> 
+    `;
+  }
 }
